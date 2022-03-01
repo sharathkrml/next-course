f7bca7e65888f05048a0c1ca28df08a2cd0af920
sharath V <sharathkrml@gmail.com>
2022-03-01 19:25:37 +0530

    added basic codes

--- pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-function details() {
-  const router = useRouter();
-  console.log(router.query.newsId);
-  return <div>Variety</div>;
-}
-
-export default details;
